983c2240011d8c34c5284ca3302bc6b3c8d9e59b
Mysterken <Mysterkenzo@gmail.com>
2021-10-26 15:16:02 +0200

    First commit

---
+++ Player.php
@@ -0,0 +1,62 @@
+<?php
+
+class Player
+{
+
+    public function __construct($hp, $defense, $attack)
+    {
+        $this->hp = $hp;
+        $this->defense = $defense;
+        $this->attack = $attack;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAttack()
+    {
+        return $this->attack;
+    }
+
+    /**
+     * @param mixed $attack
+     */
+    public function setAttack($attack)
+    {
+        $this->attack = $attack;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDefense()
+    {
+        return $this->defense;
+    }
+
+    /**
+     * @param mixed $defense
+     */
+    public function setDefense($defense)
+    {
+        $this->defense = $defense;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHp()
+    {
+        return $this->hp;
+    }
+
+    /**
+     * @param mixed $hp
+     */
+    public function setHp($hp)
+    {
+        $this->hp = $hp;
+    }
+
+
+}
